1eda6d5f763136d07232d5882130cb47312b220e
evgeniygalagurya <evgeniygalagurua@gmail.com>
2023-05-25 19:30:03 +0300

    make it better19.0

--- src/main/java/HerokuEnv.java
+++ src/main/java/HerokuEnv.java
@@ -4,7 +4,7 @@ public class HerokuEnv {
         try {
             return Integer.parseInt(System.getenv("PORT"));
         } catch (NumberFormatException ex) {
-            return 5000;
+            return 3000;
         }
     }
 
