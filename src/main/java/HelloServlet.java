f9b981b662e4247c9953695de09368300d46f3b6
evgeniygalagurya <evgeniygalagurua@gmail.com>
2023-05-28 09:03:53 +0300

    ver.linux12

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -23,50 +23,10 @@ public class HelloServlet extends HttpServlet {
   }
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//    try (Writer w = resp.getWriter()) {
-//      w.write(helloMessage);
-//    }
-    Configuration cfg = new Configuration(Configuration.VERSION_2_3_32);
-    cfg.setDefaultEncoding(String.valueOf(StandardCharsets.UTF_8));
-    cfg.setDirectoryForTemplateLoading(new File(ResourcesOps.dirUnsafe("tpl")));
-    HashMap<String, Object> data = new HashMap<>();
-    try {
-      Users.usersList();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-
-    i = i + 1;
-    try {
-      if (i > Users.mapUsers.quantity()){
-        i = 1;
-      }
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-    String userName;
-    try {
-      userName = Users.mapUsers.load(i).name();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-    String userLink;
-    try {
-      userLink = Users.mapUsers.load(i).link();
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-
-    data.put("name", userName);
-    data.put("linkpage", userLink);
-
-    try (PrintWriter w = resp.getWriter()) {
-      cfg
-              .getTemplate("users-page.ftl")
-              .process(data, w);
-    } catch (TemplateException x) {
-      throw new RuntimeException(x);
+    try (Writer w = resp.getWriter()) {
+      w.write(helloMessage);
     }
   }
+
 
 }
